902256cce7b88a4d0cff45aae1eb4a5ddea3bcf8
AbnHL-24 <abneralberto-hernandezlopez@cunoc.edu.gt>
2019-05-03 13:13:00 -0600

    creacion de ventanas a trabajar

--- FinalProject/src/Main.java
+++ FinalProject/src/Main.java
@@ -1,3 +1,6 @@
+
+import ui.UI;
+
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
@@ -10,7 +13,9 @@
  */
 public class Main {
     public static void main(String[] args) {
-        
+        UI ui = new UI();
+        ui.setVisible(true);
+        ui.setLocationRelativeTo(null);
     }
     
 }
